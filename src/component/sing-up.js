80c1d20b5370d3da6ff611dac0a71c1ff50852d5
ZhekaLited <zhenyagorovtsov@gmail.com>
2022-07-28 20:14:35 +0300

    111

--- src/component/sing-up.js
+++ src/component/sing-up.js
@@ -43,3 +43,31 @@ function isEmailValid(value = '') {
 
     return value.trim()
 }
+
+function isPasswordValid(value = '') {
+    const counter = {
+        letter: 0,
+        digit: 0,
+        upperCaseLetter:0
+    }
+
+    const array = value.split('')
+    array.forEach(symbol => {
+        if (typeof symbol === 'string' && !isFinite(symbol)) {
+            counter.letter++
+        }
+        if (isFinite(symbol)) {
+            if(symbol !== ' '){
+                counter.digit++
+            }
+        }
+        if (!isFinite((symbol) && symbol === symbol.toUpperCase()) {
+            counter.upperCaseLetter++
+        }
+     }
+    )
+            for (let key in counter) {
+            if (counter[key] === 0) return false
+            }
+        return value
+}
--- src/core/form.js
+++ src/core/form.js
@@ -20,11 +20,38 @@ export class Form {
             let isValid = true
             validators.forEach(validator => {
                 isValid = validator(this.form[field].value) && isValid
+                console.log(this.form[field])
             })
+
+            !isValid ? setNoticeError(this.form[field]) : cosole.log('валидно')
 
             isValidateForm = isValid && isValidateForm
 
         })
         return isValidateForm
     }
+}
+
+function  setNoticeError(input) {
+
+ input.parentElement.classList.add('invalid')
+
+    if (input.getAttribute('name') === 'name') {
+        input.insertAdjacentHTML('afterend', setErrorText('Field is required'))
+
+    }
+    if (input.getAttribute('name') === 'email') {
+        input.insertAdjacentHTML('afterend', setErrorText('Field is required ' 
+            '(at: least: "@' symbol)'))
+
+    }
+    if (input.getAttribute('name') === 'password') {
+        input.insertAdjacentHTML('afterend', setErrorText('Field is required
+             + '(at: least: 1  letter , 1 diggit, 1 uppercase letter)'))
+
+    }
+}
+
+function setErrorText(text) {
+    return `<p class="form__field-warning">${text}</p>`
 }
